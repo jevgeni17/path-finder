e77ad3a56841e5a4aee3ae2996470fa8214792f5
Jevgeni Barkin <zekaxdpro@gmail.com>
2020-02-13 00:00:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 class Graph():
     def __init__(self):
         """
-        self.edges is a dict of all possible next nodes
+         self.edges is a dict of all possible next nodes
         e.g. {'X': ['A', 'B', 'C', 'E'], ...}
         self.weights has all the weights between two nodes,
         with the two nodes as a tuple as the key
